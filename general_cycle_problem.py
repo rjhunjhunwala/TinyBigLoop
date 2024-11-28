dcd6d40671f29818bb886874a9669845a7408bf6
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2024-11-28 17:03:12 -0500

    Draw output

--- general_cycle_problem.py
+++ general_cycle_problem.py
@@ -4,7 +4,7 @@ from mip import Model, xsum, maximize, BINARY
 
 from typing import List
 
-SIZE = 25
+SIZE = 16
 
 V = [(i, j) for i in range(SIZE) for j in range(SIZE)]
 
@@ -18,9 +18,47 @@ for i, j in V:
                 if (0 <= new_i < SIZE and 0 <= new_j < SIZE):
                     E[((i, j), (new_i, new_j))] = 1
                     E[((new_i, new_j), (i, j))] = 1
+                
+
+def draw_tour(locations):
+    import turtle
+
+    # List of locations (example tuples)
+    locations = [(0, 0), (1, 2), (3, 3), (4, 1), (2, -1)]  # Modify this with your list
+
+    # Set up the screen
+    screen = turtle.Screen()
+    screen.bgcolor("white")
+
+    # Create a turtle object
+    pen = turtle.Turtle()
+    pen.speed(5)
+    pen.pensize(2)  # Set the thickness of the line to 2 pixels
+    pen.color("red")
+
+    # Set the scaling factor for the points
+    scaling_factor = 16
+
+    # Move the turtle to the starting point
+    pen.penup()
+    pen.goto(locations[0][0] * scaling_factor, locations[0][1] * scaling_factor)
+    pen.pendown()
+
+    # Loop through the locations and draw the line
+    for x, y in locations:
+        pen.goto(x * scaling_factor, y * scaling_factor)
+
+    # Connect the last point back to the first point to close the loop
+    pen.goto(locations[0][0] * scaling_factor, locations[0][1] * scaling_factor)
+
+    # Hide the turtle after drawing
+    pen.hideturtle()
+
+    # Keep the window open until clicked
+    screen.exitonclick()
 
 
-def find_longest_tour(V, E) -> List[V]:
+def find_longest_tour(V, E) -> List:
     n = len(V)
     model = Model()
 
@@ -55,16 +93,20 @@ def find_longest_tour(V, E) -> List[V]:
 
     # checking if a solution was found
     if model.num_solutions:
+        out_list = []
         s = nc = max(V, key = lambda v: is_magic[v].x)
         print("Len found " + str(model.objective_value))
         print(nc)
+        out_list.append(nc)
         while True:
 
             nc = [i for i in V if (nc, i) in E and did_use_edge[(nc, i)].x >= 0.99][0]
             print(nc)
+            out_list.append(nc)
             # out.write(' -> %s' % nc)
             if nc == s:
                 break
+        draw_tour(out_list)
         out.write('\n')
 
 
